de74b46ebdda283db559c317413be0fc6719ae49
Dang Nhat <88953246+dangnhatsimon@users.noreply.github.com>
2025-04-05 08:13:25 +0000

    ok

---
+++ config/config.py
@@ -0,0 +1,17 @@
+config = {
+    "openai": {
+        "api_key": ""
+    },
+    "kafka": {
+        "sasl.username": "",
+        "sasl.password": "",
+        "bootstrap.servers": "",
+        "security.protocol": "SASL_SSL",
+        "sasl.mechanisms": "PLAIN",
+        "session.timeout.ms": 50000
+    },
+    "schema_registry": {
+        "url": "",
+        "basic.auth.user.info": ""
+    }
+}
