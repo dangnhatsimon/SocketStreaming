98f33cdcf44f2176016085ff8eea3cb808f62be2
Dang Nhat <88953246+dangnhatsimon@users.noreply.github.com>
2025-04-07 10:29:51 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,4 +1,17 @@
-configuration = {
-    "AWS_ACCESS_KEY": "",
-    "AWS_SECRET_KEY": ""
+config = {
+    "openai": {
+        "api_key": "key"
+    },
+    "kafka": {
+        "sasl.username": "",
+        "sasl.password": "",
+        "bootstrap.servers": "",
+        "security.protocol": "SASL_SSL",
+        "sasl.mechanisms": "PLAIN",
+        "session.timeout.ms": 50000
+    },
+    "schema_registry": {
+        "url": "",
+        "basic.auth.user.info": ""
+    }
 }
